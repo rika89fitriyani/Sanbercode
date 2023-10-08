f609bed8f95af3363201a597e676716f8aae8b17
rika89fitriyani <147331088+rika89fitriyani@users.noreply.github.com>
2023-10-09 00:33:05 +0700

    Add files via upload

---
+++ soal1.js
@@ -0,0 +1,25 @@
+//program dari akar pangkat 2 dari x dengan x harus bilangan genap
+const number1 = 0;
+const number2 = 2;
+const number3 = 4;
+const number4 = 6;
+const number5 = 8;
+const number6 = 10;
+
+const result1 = Math.sqrt(number1);
+const result2 = Math.sqrt(number2);
+const result3 = Math.sqrt(number3);
+const result4 = Math.sqrt(number4);
+const result5 = Math.sqrt(number5);
+const result6 = Math.sqrt(number6);
+
+if(number1 > 0){
+    document.write("Tidak bisa input bilangan negatif");
+}
+
+console.log(`Akar pangkat dua dari ${number1} adalah ${result1}`);
+console.log(`Akar pangkat dua dari ${number2} adalah ${result2}`);
+console.log(`Akar pangkat dua dari ${number3} adalah ${result3}`);
+console.log(`Akar pangkat dua dari ${number4} adalah ${result4}`);
+console.log(`Akar pangkat dua dari ${number5} adalah ${result5}`);
+console.log(`Akar pangkat dua dari ${number6} adalah ${result6}`);
